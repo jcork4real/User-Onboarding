ed4651c8a909ffc03d721cbb046360b68320db66
Jarone McCorkle <jaroneemccorkle@gmail.com>
2020-06-11 03:14:46 -0700

    implementing form validation and error mssging

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -6,6 +6,26 @@ import * as yup from 'yup';
 
 function Form(){
 
+    // managing state for our form inputs
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    password: "",
+    terms: ""
+  });
+
+  //Step 2 form validation and error messaging
+  // state for our errors
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    password: "",
+    terms: ""
+  });
+
+
+
+// Form Start
 <form>
     <label htmlFor="name">
     Name
