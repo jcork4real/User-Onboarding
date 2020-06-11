8f4844374638575afd1e82791b081662b3e6df75
Jarone McCorkle <jaroneemccorkle@gmail.com>
2020-06-11 02:56:59 -0700

    initial form inputs complete

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -6,6 +6,42 @@ import * as yup from 'yup';
 
 function Form(){
 
+<form>
+    <label htmlFor="name">
+    Name
+    <input
+      type="text"
+      name="name"
+    />
+  </label>
+    <label htmlFor= "email">
+        Email
+        <input
+            type="text"
+            name="email"
+        />
+
+    <label htmlFor="password">
+        Password
+        <input
+            type="text"
+            name="password"
+        />
+
+    </label>
+
+   <label htmlFor="terms" className="terms">
+         Terms & Conditions
+    <input
+      type="checkbox"
+      name="terms"
+      checked={true} 
+    />
+    
+  </label>
+    <button>Submit</button>
+    </label>
+ </form>
 }
 
 export default Form;
